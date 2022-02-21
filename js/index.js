e480b4ceedce97aa35cd73aff4a28dfa130bc1b5
joakimlees <91544170+joakimlees@users.noreply.github.com>
2022-02-21 17:07:48 +0100

    made changes
    currently trying to link to details.html

--- js/index.js
+++ js/index.js
@@ -2,17 +2,23 @@
 
 const url = "https://rickandmortyapi.com/api/character";
 
+const characterContainer = document.querySelector(".character-container");
+
 async function getApi() {
   const response = await fetch(url);
   const result = await response.json();
   const characters = result.results;
   addCharacters(characters);
+  console.log(characters);
 }
 
 getApi();
 
 function addCharacters(character) {
   character.forEach((character) => {
-    console.log(character);
+    characterContainer.innerHTML += `<div><b>Name: </b>${character.name}</div>
+                                      <div><b>Star in: </b> ${character.episode.length} episodes</div>
+                                        <img src="${character.image}">
+                                        <a href="details.html?page=${character.id}">click here</a>`;
   });
 }
